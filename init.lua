27241a92cb0ea9ec40578e5f410d92e683ea6a48
Kewzen <rikochet333.top@gmail.com>
2023-04-17 11:15:14 +0300

    config*
    git commit -m config*

--- init.lua
+++ init.lua
@@ -3,8 +3,11 @@ require('maps')
 require('highlights')
 require('windows')
 require('plug')
-require('coc')
 require('fileexp')
+require('lsp')
+require('telescope')
 
 
-vim.g.python3_host_prog="/usr/bin/python3.10"
+vim.g.netrw_banner = 0
+vim.g.netrw_liststyle = 3
+vim.g.netrw_browse_split = 3
--- lua/plug.lua
+++ lua/plug.lua
@@ -3,11 +3,20 @@ local Plug = vim.fn['plug#']
 vim.call('plug#begin', '~/AppData/Local/nvim/autoload/plugged')
 Plug 'hoob3rt/lualine.nvim'
 Plug 'preservim/nerdtree'
-Plug('neoclide/coc.nvim', {branch = 'release'})
 Plug 'https://github.com/rafi/awesome-vim-colorschemes'
 Plug 'svrana/neosolarized.nvim'
 Plug 'tjdevries/colorbuddy.nvim'
 Plug 'kyazdani42/nvim-web-devicons'	
 Plug 'flazz/vim-colorschemes'
+Plug 'neovim/nvim-lspconfig'
+Plug 'hrsh7th/nvim-cmp'
+Plug 'hrsh7th/cmp-nvim-lsp'
+Plug 'saadparwaiz1/cmp_luasnip'
+Plug 'L3MON4D3/LuaSnip'
+Plug 'nvim-lua/plenary.nvim'
+Plug 'nvim-telescope/telescope.nvim'
+
+
+
 vim.call('plug#end')
 
